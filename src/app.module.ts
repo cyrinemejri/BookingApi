a111bf10171a3c2c30f05b8cbaa1dc7809e7eb5a
cyrinemejri <sirinemejri@insat.ucar.tn>
2024-10-10 18:18:04 +0100

    booking

---
+++ src/app.module.ts
@@ -0,0 +1,61 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReservationsModule } from './reservations/reservations.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ListingsModule } from './listings/listings.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { RatingsModule } from './ratings/ratings.module';
+import { CommentsModule } from './comments/comments.module';
+import { ChatGateway } from './chat/chat.gateway';
+import { ConversationsModule } from './conversations/conversations.module';
+import { MessagesModule } from './messages/messages.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { SseModule } from './sse/sse.module';
+import * as dotenv from 'dotenv';
+import { PrometheusModule } from "@willsoto/nestjs-prometheus";
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './logging.interceptor';
+
+dotenv.config();
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST,
+      port: parseInt(process.env.PORT),
+      username: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true,
+      autoLoadEntities: true,
+      // logging: true,
+    }),
+    ReservationsModule,
+    UsersModule,
+    AuthModule,
+    ListingsModule,
+    NotificationsModule,
+    RatingsModule,
+    CommentsModule,
+    ConversationsModule,
+    MessagesModule,
+    EventEmitterModule.forRoot(),
+    SseModule,
+    PrometheusModule.register()
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    ChatGateway,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor
+    }
+  ],
+})
+export class AppModule { }
